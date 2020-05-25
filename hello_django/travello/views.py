f77228ac6882da2a599237b32c1ed31120bb413e
DKEnterprises <damareshwarakumar@gmail.com>
2020-05-25 21:03:18 +0530

    may 25

--- hello_django/travello/views.py
+++ hello_django/travello/views.py
@@ -9,5 +9,22 @@ def index(request):
     dest1.price = 700
     dest1.name = 'Mumbai'
     dest1.desc = "The city that never sleeps"
+    dest1.img = 'destination_1.jpg'
 
-    return render(request, 'index.html', {'dest1': dest1})
+    dest2 = Destination()
+    dest2.id = 2
+    dest2.price = 650
+    dest2.name = 'Hyderabad'
+    dest2.desc = "First Birynai, then shervani"
+    dest2.img = 'destination_2.jpg'
+
+    dest3 = Destination()
+    dest3.id = 3
+    dest3.price = 750
+    dest3.name = 'Bengaluru'
+    dest3.desc = "The Tech City"
+    dest3.img = 'destination_3.jpg'
+
+    dests = [dest1, dest2, dest3]
+
+    return render(request, 'index.html.j2', {'dests': dests})
