b2030394aa0c83d8caad4659ead1e4a67a39fddc
DKEnterprises <damareshwarakumar@gmail.com>
2020-05-25 19:30:13 +0530

    Dynamic data using objects

--- hello_django/travello/views.py
+++ hello_django/travello/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'index.html')
+    dest1 = Destination()
+    dest1.id = 1
+    dest1.price = 700
+    dest1.name = 'Mumbai'
+    dest1.desc = "The city that never sleeps"
+
+    return render(request, 'index.html', {'dest1': dest1})
