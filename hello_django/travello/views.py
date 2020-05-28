86a6d1e6b162626c7480808a7cd90651761b04ab
DKEnterprises <damareshwarakumar@gmail.com>
2020-05-28 13:43:42 +0530

    Retreiving data from database

--- hello_django/travello/views.py
+++ hello_django/travello/views.py
@@ -4,30 +4,5 @@ from .models import Destination
 
 
 def index(request):
-    dest1 = Destination()
-    dest1.id = 1
-    dest1.price = 700
-    dest1.name = 'Mumbai'
-    dest1.desc = "The city that never sleeps"
-    dest1.img = 'destination_1.jpg'
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.id = 2
-    dest2.price = 650
-    dest2.name = 'Hyderabad'
-    dest2.desc = "First Birynai, then shervani"
-    dest2.img = 'destination_2.jpg'
-    dest2.offer = True
-
-    dest3 = Destination()
-    dest3.id = 3
-    dest3.price = 750
-    dest3.name = 'Bengaluru'
-    dest3.desc = "The Tech City"
-    dest3.img = 'destination_3.jpg'
-    dest3.offer = False
-
-    dests = [dest1, dest2, dest3]
-
+    dests = Destination.objects.all()
     return render(request, 'index.html.j2', {'dests': dests})
