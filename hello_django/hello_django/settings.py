daf154ccf0f9dc9710facc935469b1807f48f269
DKEnterprises <damareshwarakumar@gmail.com>
2020-05-28 11:54:55 +0530

    DataBase Connectivity

--- hello_django/hello_django/settings.py
+++ hello_django/hello_django/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,10 +75,20 @@ WSGI_APPLICATION = 'hello_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hello_django',
+        'USER': 'postgres',
+        'PASSWORD': 'toor',
+        'HOST': '127.0.0.1'
     }
 }
 
--- hello_django/travello/views.py
+++ hello_django/travello/views.py
@@ -10,6 +10,7 @@ def index(request):
     dest1.name = 'Mumbai'
     dest1.desc = "The city that never sleeps"
     dest1.img = 'destination_1.jpg'
+    dest1.offer = False
 
     dest2 = Destination()
     dest2.id = 2
@@ -17,6 +18,7 @@ def index(request):
     dest2.name = 'Hyderabad'
     dest2.desc = "First Birynai, then shervani"
     dest2.img = 'destination_2.jpg'
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.id = 3
@@ -24,6 +26,7 @@ def index(request):
     dest3.name = 'Bengaluru'
     dest3.desc = "The Tech City"
     dest3.img = 'destination_3.jpg'
+    dest3.offer = False
 
     dests = [dest1, dest2, dest3]
 
